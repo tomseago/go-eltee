6c047c28821deb883a8ae81c9dbcccfd03f55335
Tom Seago <tom@tomseago.com>
2019-05-21 17:14:59 -0700

    Adding react web interface

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -1,46 +1,18 @@
-var gulp = require('gulp');
-var pug = require('gulp-pug');
-var stylus = require('gulp-stylus');
-var minifyCSS = require('gulp-csso');
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
-//var watch = require("gulp-watch");
+const gulp = require("gulp");
+const gulpParcel = require("gulp-parcel");
 
-var DEST_BASE = "./root";
+const OUT = "./root";
 
-gulp.task('lib', function(){
-  return gulp.src("lib/*")
-    .pipe(gulp.dest(DEST_BASE+"/js"))
-});
+function tParcelProd() {
+    process.env.NODE_ENV = "production";
+    return gulp.src("./src/index.html", {read:false})
+        .pipe(gulpParcel({
+            outDir: `${OUT}`,
+            cache: false,
+        }));
+}
 
-gulp.task('html', function(){
-  return gulp.src('html/*.pug')
-    .pipe(pug({
-        verbose: true
-    }))
-    .pipe(gulp.dest(DEST_BASE))
-});
 
-gulp.task('css', function(){
-  // return gulp.src('css/*.less')
-  //   .pipe(less())
-  //   .pipe(minifyCSS())
-  //   .pipe(gulp.dest('build/css'))
-  return gulp.src('css/*.styl')
-    .pipe(stylus({
-      'include css': true
-    }))
-    .pipe(minifyCSS())
-    .pipe(gulp.dest(DEST_BASE)); 
-});
-
-gulp.task('js', function(){
-  return gulp.src('javascript/*.js')
-    .pipe(sourcemaps.init())
-    .pipe(concat('app.min.js'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(DEST_BASE+"/js"))
-});
-
-gulp.task('default', [ 'lib', 'html', 'css', 'js' ]);
-          
+module.exports = {
+    parcelProd: tParcelProd,
+}
